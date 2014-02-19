29c64401e798d437f1de41a466e8dd209fc0dbca
StephenGrider <ste.grider@gmail.com>
2014-02-18 17:39:11 -0800

    Initial Commit

---
+++ BlogReaderScratch/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  BlogReaderScratch
+//
+//  Created by stephen g on 2/18/14.
+//  Copyright (c) 2014 stephen g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReaderScratch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReaderScratch
+//
+//  Created by stephen g on 2/18/14.
+//  Copyright (c) 2014 stephen g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKAppDelegate class]));
+    }
+}
